6ca8718607c8b828f779086960167d82a89df913
Thalia <thalia.rowan@gmail.com>
2020-11-05 00:51:00 -0400

    created db and populated it with seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon burger", true);
+
