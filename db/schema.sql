0356e3ee5dbfa2e9d9cd339bd22f0c66d05f39f5
ThaliaRowan <thalia.rowan@gmail.com>
2020-11-18 22:10:19 -0400

    fixed db, added comma on table

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ USE burgers_db
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL,
     burger_name varchar(60) NOT NULL,
-    devoured BOOLEAN
-    PRIMARY KEY(id);
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
 );
